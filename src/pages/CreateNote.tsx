e1d5760e7d173093f429b0d06508db20b86e5081
AlexKharin <A.Harin_32v@ngknn.ru>
2023-06-14 15:05:51 +0300

    v1

--- src/pages/CreateNote.tsx
+++ src/pages/CreateNote.tsx
@@ -1,6 +1,18 @@
+import {Link} from "react-router-dom"
+import {IoIosArrowBack} from "react-icons/io"
+
 const CreateNote = () => {
   return (
-    <div>Create</div>
+    <section>
+      <header className="create-note__header">
+        <Link to="/" className="btn"><IoIosArrowBack /></Link>
+        <button className="btn lg primary">Save</button>
+      </header>
+      <form className="create-note__form">
+        <input type="text" placeholder="Title" autoFocus/>
+        <textarea rows={10} placeholder="Node details..."></textarea>
+      </form>
+    </section>
   )
 }
 
--- src/pages/Notes.tsx
+++ src/pages/Notes.tsx
@@ -9,7 +9,7 @@ const Notes = () => {
     <section>
         <header className="notes__header">
             <h2>Notes</h2>
-            <input type="text" autoFocus placeholder="Keyword..." />
+            {/* <input type="text" autoFocus placeholder="Keyword..." /> */}
             <button className='btn'><CiSearch /></button>
         </header>
         <div className="notes__container">
